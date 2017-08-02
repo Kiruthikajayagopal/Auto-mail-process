a78e5d773b12a2270bec730a14cff34dc07e19d2
sasikumar <sasi.kumar@maplebots.com>
2017-08-02 12:33:29 +0530

    Third Correction

--- Seperate Programs /Format Doc/pretty-data/test/test_xml.js
+++
@@ -1,9 +0,0 @@
-	
-var xml = '  ',
-    pp_xml  = require('../pretty-data').pd.xml(xml),
-    pp_xmlmin_com  = require('../pretty-data').pd.xmlmin(xml,true),
-    pp_xmlmin  = require('../pretty-data').pd.xmlmin(xml);
-
-console.log('\n==============================================================================\n');
-console.log('\n/*------- Original XML string: -------*/\n\n' + xml + '\n');
-console.log('\n/*------- Beautified XML -------------*/\n\n' + pp_xml  + '\n');
